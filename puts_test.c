2388ee54a462edb67a268918c03fb6b2f4e3f90d
littlemalcolm <babajideyusuf11@gmail.com>
2023-07-04 07:44:37 +0000

    readme

---
+++ puts_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n\n\n\n");
+	printf("To solve bilateral problems\n");
+
+	return(0);
+}
